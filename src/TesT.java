b292f83d35ca68c4a18be19e083bc0984d31d120
Jiliac <v.manes94@gmail.com>
2014-01-12 20:08:21 +0100

    set frontiere

--- src/TesT.java
+++ src/TesT.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +11,7 @@ public class TesT {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		File file = new File("_MG_0143.JPG");
 		try {
 			getImageColor(file);
@@ -24,29 +23,52 @@ public class TesT {
 	}
 
 	public static void getImageColor(File imagePath) throws IOException {
-		BufferedImage image;
-		ArrayList<Point> type1 = new ArrayList<Point>(), type2 = new ArrayList<Point>();
 
-		image = ImageIO.read(imagePath);
+		// I - recupere deux categorie (on pourrait en faire plus)
 
+		BufferedImage image = ImageIO.read(imagePath);
+		int categorie[][] = new int[image.getWidth()][image.getHeight()];
+
+		
 		int color;
 		for (int r = 0; r < image.getHeight(); r += 1) {
 			for (int c = 0; c < image.getWidth(); c += 1) {
 				color = image.getRGB(c, r);
-				Point p = new Point(c,r);
-				if( color > -4000000)
-					type1.add(p);
+				if (color > -4000000)
+					categorie[c][r] = 1;
 				else
-					type2.add(p);					
+					categorie[c][r] = 2;
 			}
 		}
-		for(Point p : type1){
-			image.setRGB(p.getPosX(), p.getPosY(), -4000000);
+
+		// II - set une frontiere
+
+		ArrayList<Point> frontiere = new ArrayList<Point>();
+
+		int i = categorie[0][0];
+		for (int y = 0; y < categorie.length; y++) {
+			for (int x = 0; x < categorie[y].length; x++) {
+				if (i != categorie[x][y]) {
+					frontiere.add(new Point(x, y));
+					i = categorie[x][y];
+				}
+			}
 		}
-		for(Point p : type2){
-			image.setRGB(p.getPosX(), p.getPosY(), 0);
-		}
-		
+
+		/*
+		 * III - comparer a une forme
+		 * 
+		 * 
+		 * III - preambule: pour determiner la dimension generale des formes
+		 * auxquelles on compare il faudrait creer un indice de dispersion generale
+		 * 
+		 * III - a - pour chaque forme, pour chaque point, on le raproche d'un
+		 * point de la forme; on associe une distance à ce point
+		 * 
+		 * III - b - on calcule un indice de "distance" à la forme est on
+		 * determine la forme qui est la plus proche
+		 */
+
 	}
 
 }
