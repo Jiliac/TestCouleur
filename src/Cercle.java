ca553da980ab247922771766c3400919368f508c
Jiliac <v.manes94@gmail.com>
2014-01-13 19:52:26 +0100

    Ajout de la comparaison de frontiere aux cercles

---
+++ src/Cercle.java
@@ -0,0 +1,14 @@
+
+public class Cercle extends FormeStandard{
+	int rayon;
+	
+	public Cercle(int rayon){
+		this.rayon = rayon;
+	}
+	
+	public int getDistanceProjete(Point p) {
+		int rayonPoint = p.distance(new Point(0,0));
+		return Math.abs(rayonPoint-rayon);
+	}
+
+}
--- src/ListePoints.java
+++ src/ListePoints.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 public class ListePoints extends ArrayList<Point> {
-	
+
 	/******* getter des carac' simple de la liste *******/
-	
+
 	public Point getCentre() {
 		long sommeX = 0, sommeY = 0;
 		for (Point p : this) {
@@ -21,26 +21,26 @@ public class ListePoints extends ArrayList<Point> {
 			sommeRayon += centre.distance(p);
 		return (int) (sommeRayon / this.size());
 	}
-	
-	/***********  calcule de la meilleurs forme ************/
-	
-	public int comparaison(FormeStandard fs){
+
+	/*********** calcule de la meilleurs forme ************/
+
+	public int comparaison(FormeStandard fs) {
 		ListePoints listeToAnalyse = this.centrageListe();
-		int sommeDiff =0;
-		for(Point p : listeToAnalyse)
+		int sommeDiff = 0;
+		for (Point p : listeToAnalyse)
 			sommeDiff += fs.getDistanceProjete(p);
-		return (int) (sommeDiff/this.size());
+		return (int) (sommeDiff / this.size());
 	}
 	
-	public ListePoints centrageListe(){
+	public int comparaisonCercle(){
+		return this.comparaison(new Cercle(this.getRayon()));
+	}
+	
+	public ListePoints centrageListe() {
 		Point centre = this.getCentre();
 		ListePoints lp = new ListePoints();
-		for(Point p : this)
+		for (Point p : this)
 			lp.add(p.moins(centre));
 		return lp;
-		
 	}
-	
-	
-	
 }
