366115bc9e9c8e48bd7f6baaa3da85134f1bd7d5
Jiliac <v.manes94@gmail.com>
2014-01-17 15:44:59 +0100

    Implementation de ce qu'a fait Xiao et calcul des frontières

---
+++ src/Segmentation.java
@@ -0,0 +1,97 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Segmentation {
+
+	public BufferedImage segmenter() {
+		BufferedImage img = null;// Création d'une image BufferedImage
+		try {
+			// la méthode statique read de la classe javax.imageio.ImageIO
+			// renvoie
+			// une instance de la classe BufferedImage (qui étend la classe
+			// abstraite Image).
+			img = ImageIO.read(new File("test8.jpg"));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (int x = 0; x < img.getWidth(); x++) {
+			for (int y = 0; y < img.getHeight(); y++) {// parcourir l'image
+				Color c = new Color(img.getRGB(x, y));// prendre des valeurs RGB
+														// de chaque pixel
+				int r = c.getRed();
+				int g = c.getGreen();
+				int b = c.getBlue();
+
+				float[] hsb = Color.RGBtoHSB(r, g, b, null); // convertir RGB en
+																// HSB
+				float h = hsb[0]; // prendre la valeur teinte
+
+				if (h < 0.35 && h > 0.12) {
+					System.out.println("h change" + h);
+					int rgb = new Color(0, 255, 0).getRGB();
+					img.setRGB(x, y, rgb); // remplir cette zone par une couleur
+											// homogène
+
+				}
+			}
+		}
+
+		File outputfile = new File("output8_1.jpg");
+		try {
+			ImageIO.write(img, "jpg", outputfile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return img;
+	}
+
+	public ListePoints getFrontiere() {
+		
+		// I - recupere deux categorie (on pourrait en faire plus)
+		
+		BufferedImage img = this.segmenter();
+		int categorie[][] = new int[img.getWidth()][img.getHeight()];
+		
+		
+		for (int x = 0; x < img.getWidth(); x++) {
+			for (int y = 0; y < img.getHeight(); y++) {// parcourir l'image
+				Color c = new Color(img.getRGB(x, y));// prendre des valeurs RGB
+														// de chaque pixel
+				int r = c.getRed();
+				int g = c.getGreen();
+				int b = c.getBlue();
+
+				float[] hsb = Color.RGBtoHSB(r, g, b, null); // convertir RGB en
+																// HSB
+				float h = hsb[0]; // prendre la valeur teinte
+
+				if (h < 0.35 && h > 0.12) 
+					categorie[x][y] = 1;
+				else
+					categorie[x][y] = 2;
+			}
+		}
+		
+		// II - set une frontiere
+
+		ListePoints frontiere = new ListePoints();
+
+		int i = categorie[0][0];
+		for (int y = 0; y < categorie.length; y++) {
+			for (int x = 0; x < categorie[y].length; x++) {
+				if (i != categorie[x][y]) {
+					frontiere.add(new Point(x, y));
+					i = categorie[x][y];
+				}
+			}
+		}
+		
+		return frontiere;
+	}
+}
--- src/TesT.java
+++ src/TesT.java
@@ -1,77 +1,14 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
-
 public class TesT {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-
-		File file = new File("_MG_0143.JPG");
-		try {
-			getImageColor(file);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	
-	
-	// methode prototype du projet
-	public static void getImageColor(File imagePath) throws IOException {
-
-		// I - recupere deux categorie (on pourrait en faire plus)
-
-		BufferedImage image = ImageIO.read(imagePath);
-		int categorie[][] = new int[image.getWidth()][image.getHeight()];
-
-		
-		int color;
-		for (int r = 0; r < image.getHeight(); r += 1) {
-			for (int c = 0; c < image.getWidth(); c += 1) {
-				color = image.getRGB(c, r);
-				if (color > -4000000)
-					categorie[c][r] = 1;
-				else
-					categorie[c][r] = 2;
-			}
-		}
-
-		// II - set une frontiere
-
-		ArrayList<Point> frontiere = new ArrayList<Point>();
-
-		int i = categorie[0][0];
-		for (int y = 0; y < categorie.length; y++) {
-			for (int x = 0; x < categorie[y].length; x++) {
-				if (i != categorie[x][y]) {
-					frontiere.add(new Point(x, y));
-					i = categorie[x][y];
-				}
-			}
-		}
-
+		Segmentation seg = new Segmentation();
+		ListePoints frontiere = seg.getFrontiere();
+		int indice = frontiere.comparaisonCercle();
 		/*
-		 * III - comparer a une forme
-		 * 
-		 * 
-		 * III - preambule: pour determiner la dimension generale des formes
-		 * auxquelles on compare il faudrait creer un indice de dispersion generale
-		 * 
-		 * III - a - pour chaque forme, pour chaque point, on le raproche d'un
-		 * point de la forme; on associe une distance à ce point
-		 * 
-		 * III - b - on calcule un indice de "distance" à la forme est on
-		 * determine la forme qui est la plus proche
+		 * Là j'ai un indice qui quantifie si la liste de point est proche ou non d'un cercle
+		 * Cet indice ne demande plus qu'à être exploité
 		 */
-
 	}
+
 
 }
